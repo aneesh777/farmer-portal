943c4eaf9bce0eb0c6dd8d8cee2e26a30f9e80fe
Aneesh KS <aneesh.ks@loremine.com>
2023-02-11 23:32:43 +0530

    test

--- src/components/Crops.jsx
+++ src/components/Crops.jsx
@@ -10,7 +10,7 @@ const Crops = () => {
   <div className="mask" style={{backgroundColor: "rgba(0, 0, 0, 0.6)"}}>
     <div className="d-flex justify-content-center align-items-center h-100">
       <div className="text-white">
-        <h1 className="mb-3">Heading</h1>
+        <h1 className="mb-3">Dashboard</h1>
         <h4 className="mb-3">Subheading</h4>
         <a className="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
       </div>
